d7faeec7c7d4d5ff94717ea0375b2af66b6fa565
Jesper van den Ende <jespertheend@gmail.com>
2023-04-20 20:16:37 +0200

    feat: Add modifier keys to mouse events

--- src/FakeMouseEvent.js
+++ src/FakeMouseEvent.js
@@ -9,6 +9,11 @@ export class FakeMouseEvent extends Event {
 		this.clientX = eventInit?.clientX ?? 0;
 		this.clientY = eventInit?.clientY ?? 0;
 		this.buttons = eventInit?.buttons ?? 0;
+
+		this.altKey = eventInit?.altKey ?? false;
+		this.ctrlKey = eventInit?.ctrlKey ?? false;
+		this.metaKey = eventInit?.altKey ?? false;
+		this.shiftKey = eventInit?.shiftKey ?? false;
 	}
 }
 
